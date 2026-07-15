4fd0a1ce43442de5c36d45a6d018f789a634d1d7
ubotweb <ubot.web.id@gmail.com>
2026-07-16 05:48:16 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import honox from 'honox/vite'
+import client from 'honox/vite/client'
 import pages from '@hono/vite-cloudflare-pages'
 
-export default defineConfig({
-  plugins: [
-    honox(),
-    pages({
-      entry: 'app/server.ts'
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  // 1. Build khusus untuk sisi Client (Browser)
+  if (mode === 'client') {
+    return {
+      plugins: [client()]
+    }
+  }
+
+  // 2. Build khusus untuk sisi Server (menghasilkan _worker.js untuk Cloudflare)
+  return {
+    build: {
+      emptyOutDir: false // PENTING: Mencegah file client terhapus saat server sedang dirakit
+    },
+    plugins: [
+      honox(),
+      pages({
+        entry: 'app/server.ts'
+      })
+    ]
+  }
 })
